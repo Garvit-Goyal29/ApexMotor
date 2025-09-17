374c8790c5d99d801d37d4541b7a4fef735dd5c3
Garvit-Goyal29 <garvitpersonal29557@gmail.com>
2025-09-18 01:21:10 +0530

    fix server

--- src/backend/server.js
+++ src/backend/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT ||  5000;
 app.use(express.json());
 app.use(cors({
     origin: ["http://127.0.0.1:5500", "https://apex-motor.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type"],
 }));
 app.options("*", cors());
